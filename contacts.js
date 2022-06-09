a6b10c4ad05861425df26d8177d199425cd1c3ef
ElenaStepanchuk <podolyak365@gmail.com>
2022-06-09 16:22:44 +0300

    yargs

--- contacts.js
+++ contacts.js
@@ -1,25 +1,27 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { v4 } = require("uuid");
-
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
   const contacts = JSON.parse(data);
   return contacts;
 }
-
 async function getContactById(contactId) {
   const contacts = await listContacts();
-  const result = contacts.find((contact) => contact.id === contactId);
+  const result = contacts.find((contact) => contact.id === String(contactId));
   return result ? result : null;
 }
-
 async function removeContact(contactId) {
-  // ...твой код
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((contact) => contact.id === String(contactId));
+  if (idx === -1) {
+    return null;
+  }
+  const [remContact] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return remContact;
 }
-
 async function addContact(name, email, phone) {
   const contacts = await listContacts();
   const newContact = { id: v4(), name, email, phone };
@@ -27,7 +29,6 @@ async function addContact(name, email, phone) {
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
   return newContact;
 }
-
 async function updateContact(id, name, email, phone) {
   const contacts = await listContacts();
   const idx = contacts.findIndex((contact) => contact.id === id);
@@ -38,7 +39,6 @@ async function updateContact(id, name, email, phone) {
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
   return contacts[idx];
 }
-
 module.exports = {
   listContacts,
   getContactById,
--- index.js
+++ index.js
@@ -1,69 +1,47 @@
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  updateContact,
-} = require("./contacts.js");
-// index.js
-// const argv = require("yargs").argv;
-
-// TODO: рефакторить
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const contactsOperations = require("./contacts.js");
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      const contacts = await listContacts();
-      console.log(contacts);
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
       break;
     case "get":
-      const contact = await getContactById(id);
+      const contact = await contactsOperations.getContactById(id);
       if (!contact) {
         throw new Error(`Contact with id=${id} not found`);
       }
       console.log(contact);
       break;
     case "add":
-      const newContact = await addContact(name, email, phone);
+      const newContact = await contactsOperations.addContact(
+        name,
+        email,
+        phone
+      );
       console.log(newContact);
       break;
     case "update":
-      const updContact = await updateContact(id, name, email, phone);
+      const updContact = await contactsOperations.updateContact(
+        id,
+        name,
+        email,
+        phone
+      );
       if (!updContact) {
         throw new Error(`Contact with id=${id} not found`);
       }
       console.log(updContact);
       break;
-
-    //     case "remove":
-    //       // ... id
-    //       break;
-
+    case "remove":
+      const removeCont = await contactsOperations.removeContact(id);
+      console.log(removeCont);
+      break;
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
-
-// {
-//     "id": "42d6559f-3db9-4df3-9dec-432e216b4409",
-//     "name": "Ally Mond",
-//     "email": "n.a@com.uk",
-//     "phone": "(992) 111-1111"
-//   }
-
-// invokeAction({ action: "list" });
-// const id = "2";
-// invokeAction({ action: "get", id });
-
-// invokeAction({
-//   action: "add",
-//   name: "Ally Mond",
-//   email: "n.a@com.uk",
-//   phone: "(992) 111-1111",
-// });
-
-invokeAction({
-  action: "update",
-  id: "42d6559f-3db9-4df3-9dec-432e216b4409",
-  name: "Ally Mond77",
-  email: "n.a@com.ukraine",
-  phone: "(992) 222-2222",
-});
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
